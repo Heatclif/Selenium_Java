ae4ec3f969521a9bb9d77237ac5e3a58ae5d4d52
Vivek Biswas <vivekbiswas5@gmail.com>
2025-03-18 00:06:01 +0530

    [VTP-0001] Craeted the Base and Driver page for Selenium framework

---
+++ src/test/java/core/Driver.java
@@ -0,0 +1,43 @@
+package core;
+
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Driver {
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    private Driver() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static WebDriver getDriver(String browser) {
+        if (driver.get() == null) {
+            switch (browser.toLowerCase()) {
+                case "chrome":
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--start-maximized");
+                    options.addArguments("--disable-notifications");
+                    options.setCapability("webSocketUrl", true);    //Enable BiDi
+                    // options.addArguments("--headless");
+
+                    driver.set(new ChromeDriver(options));
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unsupported browser: " + browser);
+            }
+
+            // Implicit wait to allow elements to load
+            driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        }
+        return driver.get();
+    }
+
+    public static void quitDriver() {
+        if (driver.get() != null) {
+            driver.get().quit();
+            driver.remove();
+        }
+    }
+}
